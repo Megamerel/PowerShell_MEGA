b485bf678b4b123498c8de6b1d5f334434619e23
Megamerel <120579367+Megamerel@users.noreply.github.com>
2022-12-14 17:56:54 +0100

    some PS1
    some PS1 script

---
+++ Wlan_WiFi_Password.ps1
@@ -0,0 +1,7 @@
+﻿(netsh wlan show profiles) | Select-String “\:(.+)$” | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} |
+%{(netsh wlan show profile name=”$name” key=clear)} | Select-String “Key Content\W+\:(.+)$” | 
+%{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | 
+%{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Out-File -FilePath .\WLanPass.txt
+
+ Format-Table -AutoSize
+
